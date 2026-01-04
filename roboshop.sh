1c121b5cb0b6ec346cf4785015c1354f8a26c606
Sri Charan Reddy <charankudumurula@gmail.com>
2026-01-04 13:38:27 +0530

    Roboshoproject

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,10 @@ DOMAIN_NAME="sricharan.site"
 # for instance in ${INSTANCES[@]}
 for instance in $@
 do 
-     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03de6d8134f235f9d --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='"$instance"'}]' --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    
+     # Get Private IP
+     if [ $instance != "frontend" ]
       then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
